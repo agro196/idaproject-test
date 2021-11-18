66208056aeb613f6c53313279be05e4353cdd02c
Agro <agro_06@mail.ru>
2021-11-18 13:43:35 +0300

    Starting development

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1 class="">Главная страница приложения</h1>
+    <AppForm />
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" module></style>
